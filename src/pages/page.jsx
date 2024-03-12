12749edafc9eb6374ae18272752fcf1df60ff0e9
Mukund Kumar <sankhyan.mukund@gmail.com>
2024-03-12 16:29:26 +0530

    Add animation to Demo & Change scroll bar

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -3,20 +3,20 @@ import { About, Demo, Home, References, Team } from "../sections";
 
 const Page = () => {
     return (
-        <div className="flex flex-col w-full h-full">
-            <section id="home">
+        <div className="flex flex-col w-full h-full snap-mandatory snap-y">
+            <section id="home" className="snap-center">
                 <Home />
             </section>
-            <section id="demo">
+            <section id="demo" className="snap-center">
                 <Demo />
             </section>
-            <section id="about">
+            <section id="about" className="snap-center">
                 <About />
             </section>
-            <section id="references">
+            <section id="references" className="snap-center">
                 <References />
             </section>
-            <section id="team">
+            <section id="team" className="snap-center">
                 <Team />
             </section>
         </div>
--- src/sections/Demo.jsx
+++ src/sections/Demo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { easeInOut, motion } from "framer-motion";
 
 function Demo() {
     const [file, setFile] = useState(null);
@@ -45,45 +46,79 @@ function Demo() {
         };
     };
 
-    const showMask = () => {
-        return (
-            <div className="indicator">
-                <span className="indicator-item indicator-bottom indicator-center badge badge-primary">Mask</span>
-
-            </div>
-        )
-    }
-
     return (
         <div className="hero min-h-screen bg-base-100">
             <div className="hero-content text-neutral-content w-full h-full">
                 <div className="flex flex-col text-center justify-center items-center gap-y-6">
-                    <h1 className="mb-5 text-5xl font-bold">Demo</h1>
-                    <div className="flex w-full gap-x-6 justify-center">
-                        <input type="file" onChange={handleFileChange} className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
-                        <button className="btn btn-primary" onClick={handleUpload}>Segment</button>
-                    </div>
-                    <div className="flex w-full gap-x-2">
+                    <motion.h1
+                        className="mb-5 text-5xl font-bold"
+                        initial={{ opacity: 0, y: "10px" }}
+                        whileInView={{ opacity: 1, y: "0px" }}
+                        transition={{ ease: easeInOut, duration: 0.5 }}
+                    >
+                        Explore the Model
+                    </motion.h1>
+                    <motion.div
+                        className="flex w-full gap-x-6 justify-center"
+                        initial={{ opacity: 0, y: "10px" }}
+                        whileInView={{ opacity: 1, y: "0px" }}
+                        transition={{ ease: easeInOut, duration: 0.5 }}
+                        layout
+                    >
+                        <motion.input
+                            type="file"
+                            onChange={handleFileChange}
+                            className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+                            initial={{ opacity: 0, x: "-10px" }}
+                            whileInView={{ opacity: 1, x: "0px" }}
+                            transition={{ ease: easeInOut, duration: 0.8 }}
+                        />
+                        <motion.button
+                            className="btn btn-primary"
+                            onClick={handleUpload}
+                            initial={{ opacity: 0, x: "10px" }}
+                            whileInView={{ opacity: 1, x: "0px" }}
+                            transition={{ ease: easeInOut, duration: 0.8 }}
+                        >
+                            Run Model
+                        </motion.button>
+                    </motion.div>
+                    <motion.div className="flex w-full gap-x-2" layout>
                         <div className="grid h-auto flex-grow card bg-base-100 rounded-box place-items-center">
                             {imgUrl && (
-                                <div className="indicator">
+                                <motion.div
+                                    className="indicator shadow-lg"
+                                    initial={{ opacity: 0, x: "-10px" }}
+                                    whileInView={{ opacity: 1, x: "0px" }}
+                                    transition={{ ease: easeInOut, duration: 0.5 }}
+                                >
                                     <span className="indicator-item indicator-bottom indicator-center badge badge-primary">Input Image</span>
                                     <img className="rounded-lg" src={imgUrl} alt="Input Image" />
-                                </div>
+                                </motion.div>
                             )}
                         </div>
-                        <div className="divider divider-horizontal"></div>
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            whileInView={{ opacity: 1 }}
+                            transition={{ ease: easeInOut, duration: 0.9 }}
+                            className="divider divider-horizontal"
+                        />
                         <div className="grid h-auto flex-grow card bg-base-100 rounded-box place-items-center">
                             {imgUrl && (isLoading ? (
                                 <span className="loading loading-ball loading-lg"></span>
                             ) : (
-                                <div className="indicator">
+                                <motion.div
+                                    className="indicator shadow-lg"
+                                    initial={{ opacity: 0, x: "10px" }}
+                                    whileInView={{ opacity: 1, x: "0px" }}
+                                    transition={{ ease: easeInOut, duration: 0.5 }}
+                                >
                                     <span className="indicator-item indicator-bottom indicator-center badge badge-primary">Segmenation Mask</span>
                                     <img className="rounded-lg" src={maskUrl} alt="Segmentation Mask" />
-                                </div>
+                                </motion.div>
                             ))}
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
