b44f0ce2391ff1bdd2bafa8639ece7779ac298df
kart781 <karthikram781@gmail.com>
2024-03-13 23:35:40 +0530

    Changed the Body(to points), Linked the Button, Increased the size of the Image

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,14 +1,59 @@
 import React from "react";
+import { easeInOut, motion } from "framer-motion";
 
 function About() {
     return (
         <section id="about" className="hero min-h-screen bg-base-100 snap-center">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src='/students/image.png' className="max-w-sm rounded-lg shadow-2xl"/>
+                <motion.img
+                    src='public/students/image.png' 
+                    className="max-w-md rounded-lg shadow-2xl" 
+                    initial={{ opacity: 0, x: "10px" }}
+                    whileInView={{ opacity: 1, x: "0px" }}
+                    transition={{ ease: easeInOut, duration: 0.8 }}
+                /> 
                 <div>
-                <h1 className="text-5xl font-bold">About the Project</h1>
-                <p className="py-6">This research aims to create a DeepLabV3+ model with a ResNet50 backbone for binary segmentation of plant images, differentiating between soil and plants. The project utilizes the PyTorch deep learning library, employing strategies like Dice Loss, Adam optimizer, and early stopping during training. Key metrics, including Intersection over Union, Pixel Accuracy, and Dice Coefficient, are tracked. The training and validation datasets consist of plant images with ground truth binary mask labels from three datasets: Eschikon Wheat Segmentation (EWS), Humans in the Loop (HIL) Plant Semantic Segmentation, and Computer Vision Problems in Plant Phenotyping (CVPPP) Leaf Counting Challenge. These datasets vary in terms of image sizes, annotation styles, and plant types. The research outlines the camera specifications and settings used for data collection, emphasizing the diversity of lighting and soil conditions. The ultimate goal is to develop a robust model for accurately segmenting plant-related regions, with potential applications in agriculture, botany, and environmental sciences. The provided code demonstrates data preparation, model architecture creation, training, and evaluation using various metrics.</p>
-                <button className="btn btn-primary">Get Started</button>
+                    <motion.h1
+                        className="prose font-bold text-5xl leading-normal tracking-wide"
+                        initial={{ opacity: 0, y: "-9px" }}
+                        whileInView={{ opacity: 1, y: "0px"  }}
+                        transition={{ ease: easeInOut, duration: 0.5 }}
+                    >
+                        About the Project
+                    </motion.h1>
+                    <motion.p 
+                        className="py-6"
+                        initial={{ opacity: 0, y: "10px" }}
+                        whileInView={{ opacity: 1, y: "0px"  }}
+                        transition={{ ease: easeInOut, duration: 0.8 }}
+                    >
+                        Embark on a journey to develop DeepLabV3+ model, revolutionizing plant segmentation. 
+                        Utilizing PyTorch, we employ Dice Loss, Adam optimizer, ensuring robustness.                    
+                    </motion.p>
+                    {/* Additional Points */}
+                    <motion.ul
+                        className="py-6"
+                        initial={{ opacity: 0, y: "10px" }}
+                        whileInView={{ opacity: 1, y: "0px"  }}
+                        transition={{ ease: easeInOut, duration: 0.8 }}
+                    >
+                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Goal:</span> Develop a DeepLabV3+ model (ResNet50 backbone) for binary plant image segmentation (soil vs. plant).</li>
+                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Tools:</span> PyTorch deep learning library, Dice Loss, Adam optimizer, early stopping.</li>
+                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Evaluation:</span> Intersection over Union, Pixel Accuracy, Dice Coefficient.</li>
+                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Datasets:</span> Eschikon Wheat Segmentation (EWS), Humans in the Loop (HIL), CVPPP Leaf Counting Challenge (diverse image sizes, annotation styles, plant types).</li>
+                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Data Collection:</span> Emphasis on camera settings and diverse lighting/soil conditions.</li>
+                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Applications:</span> Agriculture, botany, and environmental sciences.</li>
+                    </motion.ul>
+                    <a href={`https://github.com/mukund-ks/DeepLabV3Plus-PyTorch`} target="_blank">
+                        <motion.button
+                        className="btn btn-primary leading-normal tracking-wide text-base"
+                        initial={{ opacity: 0, x: "10px" }}
+                        whileInView={{ opacity: 1, x: "0px" }}
+                        transition={{ ease: easeInOut, duration: 0.8 }}
+                    >
+                        Learn More
+                    </motion.button>
+                    </a>
                 </div>
             </div>
         </section>
