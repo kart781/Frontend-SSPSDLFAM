ca0fa99377f9059eeea62abf318ed146ac13fc4b
kart781 <karthikram781@gmail.com>
2024-03-13 13:31:08 +0530

    Removed Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,10 @@ function Navbar() {
     return (
         <div className="navbar bg-base-100 fixed top-0 z-50">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">GreenSeg</a>
+                <li className="btn btn-ghost text-xl"><Link to="#home">GreenSeg</Link></li>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal gap-1">
-                    <li className="leading-normal tracking-wide font-semibold text-base"><Link to="#home">Home</Link></li>
                     <li className="leading-normal tracking-wide font-semibold text-base"><Link to="#demo">Demo</Link></li>
                     <li className="leading-normal tracking-wide font-semibold text-base"><Link to="#about">About</Link></li>
                     <li className="leading-normal tracking-wide font-semibold text-base"><Link to="#references">References</Link></li>
