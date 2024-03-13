438b186883d87fb2704b852d5df91e573d342099
Mukund Kumar <sankhyan.mukund@gmail.com>
2024-03-14 00:32:24 +0530

    Adjust typography for better readability

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,53 +6,52 @@ function About() {
         <section id="about" className="hero min-h-screen bg-base-100 snap-center">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <motion.img
-                    src='public/students/image.png' 
-                    className="max-w-md rounded-lg shadow-2xl" 
+                    src='/students/image.png'
+                    className="max-w-2xl rounded-lg shadow-2xl"
                     initial={{ opacity: 0, x: "10px" }}
                     whileInView={{ opacity: 1, x: "0px" }}
                     transition={{ ease: easeInOut, duration: 0.8 }}
-                /> 
-                <div>
+                />
+                <div className="flex flex-col gap-y-5">
                     <motion.h1
                         className="prose font-bold text-5xl leading-normal tracking-wide"
                         initial={{ opacity: 0, y: "-9px" }}
-                        whileInView={{ opacity: 1, y: "0px"  }}
+                        whileInView={{ opacity: 1, y: "0px" }}
                         transition={{ ease: easeInOut, duration: 0.5 }}
                     >
                         About the Project
                     </motion.h1>
-                    <motion.p 
-                        className="py-6"
+                    <motion.p
+                        className="prose leading-normal tracking-wide"
                         initial={{ opacity: 0, y: "10px" }}
-                        whileInView={{ opacity: 1, y: "0px"  }}
+                        whileInView={{ opacity: 1, y: "0px" }}
                         transition={{ ease: easeInOut, duration: 0.8 }}
                     >
-                        Embark on a journey to develop DeepLabV3+ model, revolutionizing plant segmentation. 
-                        Utilizing PyTorch, we employ Dice Loss, Adam optimizer, ensuring robustness.                    
+                        Making use of PyTorch, this project aims to develop a robust semantic segmentation model, capable of inference in various input conditions. For interactive use, the model is accessible via frontend, faciliated with ReactJS.
                     </motion.p>
                     {/* Additional Points */}
                     <motion.ul
-                        className="py-6"
+                        className="leading-normal tracking-wide list-disc list-outside"
                         initial={{ opacity: 0, y: "10px" }}
-                        whileInView={{ opacity: 1, y: "0px"  }}
+                        whileInView={{ opacity: 1, y: "0px" }}
                         transition={{ ease: easeInOut, duration: 0.8 }}
                     >
-                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Goal:</span> Develop a DeepLabV3+ model (ResNet50 backbone) for binary plant image segmentation (soil vs. plant).</li>
-                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Tools:</span> PyTorch deep learning library, Dice Loss, Adam optimizer, early stopping.</li>
-                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Evaluation:</span> Intersection over Union, Pixel Accuracy, Dice Coefficient.</li>
-                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Datasets:</span> Eschikon Wheat Segmentation (EWS), Humans in the Loop (HIL), CVPPP Leaf Counting Challenge (diverse image sizes, annotation styles, plant types).</li>
-                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Data Collection:</span> Emphasis on camera settings and diverse lighting/soil conditions.</li>
-                        <li><span style={{ textDecoration: "underline", fontWeight: "bold" }}>Applications:</span> Agriculture, botany, and environmental sciences.</li>
+                        <li className="prose mb-1"><span className="underline font-bold">Goal:</span> Develop a DeepLabV3+ model with a ResNet50 Backbone for binary plant image segmentation.</li>
+                        <li className="prose mb-1"><span className="underline font-bold">Tools:</span> PyTorch, OpenCV, FastAPI, ReactJS, Docker, V7Labs.</li>
+                        <li className="prose mb-1"><span className="underline font-bold">Evaluation:</span> Intersection over Union (IoU), Pixel Accuracy, Dice Coefficient, Dice Loss.</li>
+                        <li className="prose mb-1"><span className="underline font-bold">Datasets:</span> Eschikon Wheat Segmentation (EWS), Humans in the Loop (HIL), CVPPP LSC.</li>
+                        <li className="prose mb-1"><span className="underline font-bold">Custom Data Collection:</span> Emphasis on camera settings and diverse lighting/soil conditions.</li>
+                        <li className="prose mb-1"><span className="underline font-bold">Applications:</span> Agriculture, botany, and environmental sciences.</li>
                     </motion.ul>
                     <a href={`https://github.com/mukund-ks/DeepLabV3Plus-PyTorch`} target="_blank">
                         <motion.button
-                        className="btn btn-primary leading-normal tracking-wide text-base"
-                        initial={{ opacity: 0, x: "10px" }}
-                        whileInView={{ opacity: 1, x: "0px" }}
-                        transition={{ ease: easeInOut, duration: 0.8 }}
-                    >
-                        Learn More
-                    </motion.button>
+                            className="btn btn-primary leading-normal tracking-wide text-base"
+                            initial={{ opacity: 0, x: "10px" }}
+                            whileInView={{ opacity: 1, x: "0px" }}
+                            transition={{ ease: easeInOut, duration: 0.8 }}
+                        >
+                            Learn More
+                        </motion.button>
                     </a>
                 </div>
             </div>
