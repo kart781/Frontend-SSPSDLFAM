a6c4b31f0e2fda8d552793f135e58357ce146b04
kart781 <karthikram781@gmail.com>
2024-03-22 05:51:09 +0530

    Updated Timeline Page

--- src/sections/Timeline.jsx
+++ src/sections/Timeline.jsx
@@ -4,9 +4,13 @@ import { easeInOut, motion } from "framer-motion";
 function Timeline() {
     return (
         <section id="timeline" className="hero min-h-screen bg-base-100 snap-center">
+            <div className='w-3/4 m-auto text-center'>
+            <div className="flex flex-row justify-around items-center w-full h-full gap-x-10" layout></div>
+            <h1 className="prose font-bold text-5xl leading-normal tracking-wide">Timeline</h1>
+            <div className="mt-10"></div>
             <ul className="timeline">
                 <li>
-                    <div className="timeline-start timeline-box">First Macintosh computer</div>
+                    <div className="timeline-start timeline-box">Model and Dataset Creation</div>
                     <div className="timeline-middle">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                     </div>
@@ -17,33 +21,34 @@ function Timeline() {
                     <div className="timeline-middle">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                     </div>
-                    <div className="timeline-end timeline-box">iMac</div>
+                    <div className="timeline-end timeline-box">Model Finetuning</div>
                     <hr className="bg-primary"/>
                 </li>
                 <li>
                     <hr className="bg-primary"/>
-                    <div className="timeline-start timeline-box">iPod</div>
+                    <div className="timeline-start timeline-box">Creating the API & Building a Website</div>
                     <div className="timeline-middle">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                     </div>
-                    <hr/>
+                    <hr className="bg-primary"/>
                 </li>
                 <li>
-                    <hr/>
+                    <hr className="bg-primary"/>
                     <div className="timeline-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                     </div>
-                    <div className="timeline-end timeline-box">iPhone</div>
+                    <div className="timeline-end timeline-box">Integrating Website with the API</div>
                     <hr/>
                 </li>
                 <li>
                     <hr/>
-                    <div className="timeline-start timeline-box">Apple Watch</div>
+                    <div className="timeline-start timeline-box">Deploying the Website</div>
                     <div className="timeline-middle">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                     </div>
                 </li>
             </ul>
+            </div>
         </section>
     )
 }
